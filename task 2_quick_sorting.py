27c7374832d67d8cecdea9171854cf500a0f8669
urgen06 <urgen06@users.noreply.github.com>
2024-01-11 02:01:57 +0545

    program completed

--- task 2_quick_sorting.py
+++ task 2_quick_sorting.py
@@ -1,6 +1,38 @@
-arr = []
+a = []
 n = int(input("Enter size of the array:"))
 print("Enter the elements:")
 for i in range(n):
-    arr.append(int(input()))
-print(arr)
+    a.append(int(input()))
+
+def partition(a,low,high):
+    pivot=a[low]
+    i=low+1
+    j=high
+    while i<j:
+        while a[i]<= pivot:
+            i+=1
+        while a[j]>pivot:
+            j-=1
+        if i<j:
+            temp = a[i]
+            a[i]=a[j]
+            a[j]=temp
+   
+    temp= a[low] 
+    a[low]= a[j]
+    a[j]=temp 
+
+    return j
+    
+def quick(a,low,high):
+    if low<high:
+        partitionIndex= partition(a,low,high)
+        quick(a,low,partitionIndex-1)
+        quick(a,partitionIndex+1,high)
+
+quick(a,0,n-1)
+print(f"Sorted array: {a}")
+
+
+
+        
